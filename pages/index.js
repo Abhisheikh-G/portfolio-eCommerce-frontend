c95d1f7b2321d5bd15d7131e4a3d187468ebbcfc
Abhisheikh-G <abhisheikhg@gmail.com>
2020-11-05 14:58:29 -0500

    Init commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Link from "next/link";
+import { startClock } from "../redux/actions";
+import Examples from "../components/Examples";
+
+const Index = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(startClock());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Next.js starter with Material-UI and Redux</h1>
+      <Examples />
+      <Link href="/show-redux-state">
+        <a>Click to see current Redux State</a>
+      </Link>
+    </>
+  );
+};
+
+export default Index;
